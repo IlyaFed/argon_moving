c70bf9ea2e1b6d916b3b01b839ef41952fdb1a76
Ilya Fedorov <ilyafedorov@Ilyas-MacBook-Air.local>
2017-03-04 00:54:33 +0300

    smth

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:
 	g++ main.cpp -o argon_moving
 	# ./argon_moving dtime max_photo photo_step max_energy energy_step wall r_kr
-	./argon_moving 100 1000 100 0 0 600 10
+	./argon_moving 10 0 100 10000 10 600 2
 	gnuplot gnuplot_energy_t.txt
 	
 big_run:
--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ public:
 		energy_step = atoi(argv[5]);
 		wall = atoi(argv[6])*1.0e-12;
 		r_kr = atoi(argv[7]);
-		printf("dtime = %5.2f fm\n", dtime*1e15);
+		printf("dtime = %e \n", dtime);
 	} else {
 		fprintf(stderr, "Not enought arguments!\n");
 	}
@@ -70,18 +70,22 @@ public:
 	double sigma;
 	double epsilon; 
 	double mass;
-	int dtime;
+	double dtime;
 	double wall[3];
 
 	double r_kr; //max rad of interraction
 
-	Sys_atom(const double* wall_get, int* number_of_atom, const double sigma_get, const double epsilon_get, const double mass_get, const int dtime_get, const double r_kr_get ){
-		for (int l = 0; l < 3; l++) wall[l] = wall_get[l];
+	Sys_atom(const double* wall_get, int* number_of_atom, const double sigma_get, const double epsilon_get, const double mass_get, const double dtime_get, const double r_kr_get ){
+		for (int l = 0; l < 3; l++) {
+			wall[l] = wall_get[l];
+			printf("wall: %e\n", wall[l]);
+		}
 		sigma = sigma_get;
 		epsilon = epsilon_get;
 		mass = mass_get;
 		dtime = dtime_get;
 		r_kr = r_kr_get;
+		printf("r_kr = %e\n", r_kr );
 		//N = (int) aa*b*c;
 		//*a = new Atom* [N]; //create massive of atoms
 		double x[3];
@@ -113,19 +117,21 @@ public:
 
 	int displace(const int tag_photo, FILE* foutput, const int time){
 		//acceleration collect
-		for (int i = 0; i < N; i++)  for (int j = 0; j < 3; j++) a[i]->accel[j] = 0;
+		for (int i = 0; i < N; i++)  for (int l = 0; l < 3; l++) a[i]->accel[l] = 0;
 
 		double r;
-		double F_r;
+		double a_r;
 		double acceleration;
 		for (int i = 0; i < N-1; i++) {
 			for (int j = i+1; j < N; j++) {
 				r = pow( pow(a[j]->coord_0[0]-a[i]->coord_0[0],2.0) + pow(a[j]->coord_0[1]-a[i]->coord_0[1],2.0) + pow(a[j]->coord_0[2]-a[i]->coord_0[2],2.0),0.5); // distance between atoms
 				if (r  < r_kr){
-					F_r = 24*epsilon*(2*pow(sigma/r,12) - pow(sigma/r, 6))/r/r/mass; // F/r
+					a_r = 24.0*epsilon*(2*pow(sigma/r,12.0) - pow(sigma/r, 6.0))/r/r/mass; // F/r
 					
 					for (int l = 0; l < 3; l++) {
-						acceleration = F_r*(a[i]->coord_0[l] - a[j]->coord_0[l]);
+						acceleration = a_r*(a[i]->coord_0[l] - a[j]->coord_0[l]);
+						//fprintf(foutput, "%d, delta, accel = %e , dtime = %e \n",time, a[i]->accel[l], dtime);
+						
 						a[i]->accel[l] -= acceleration;
 						a[j]->accel[l] += acceleration;
 					}
@@ -139,13 +145,13 @@ public:
 		
 		for (int i = 0; i < N; i ++) {
 			for (int l = 0; l < 3; l++) {
-				x[l] = 2*a[i]->coord_0[l] - a[i]->coord_p[l]+a[i]->accel[l]*dtime*dtime; //collect new coordinate
+				x[l] = 2*a[i]->coord_0[l] - a[i]->coord_p[l]+a[i]->accel[l]*pow(dtime,2.0); //collect new coordinate
 				
-				if (x[l] < 0) {
+				if (x[l] < 0.0) {
 					x[l] = -x[l];
 					a[i]->coord_0[l] = - a[i]->coord_0[l];
 				}
-				if (x[l] > 1) {
+				if (x[l] > wall[l]) {
 					x[l] = 2*wall[l] - x[l];
 					a[i]->coord_0[l] = 2*wall[l] - a[i]->coord_0[l];
 				}	
@@ -180,7 +186,7 @@ public:
 		E += T;
 		
 		T = 2.0/3/1.23e-23*T;
-
+		printf("Everything ok\n");
 		fprintf(fenergy, "%lf\t%d %lf\n", E, time, T);
 
 		if ( (E_max == 0) || (E_max < E) ) E_max = E;
@@ -207,9 +213,9 @@ int main(int argc, char* argv[]){
 	// dependense Energy by time
 
 	char *name_file = new char [100];
-	sprintf(name_file, "energy_by_time(%d).txt", int(initial.dtime*1e12));
+	sprintf(name_file, "energy_by_time(%d).txt", int(initial.dtime*1e16));
 
-	if ( (fenergy = fopen(name_file, "w")) == 0) { 
+	if (initial.max_energy &&  (fenergy = fopen(name_file, "w")) == 0) { 
 		fprintf(stderr, "Error! Can't create energy file!\n");
 		return 1;
 	}
@@ -218,10 +224,11 @@ int main(int argc, char* argv[]){
 	fprintf(fenergy, "E\ttime\t T\n");
 
 	//start
-	double wall[3] = {initial.wall, initial.wall, initial.wall};
+	double wall[3] = {initial.wall*10, initial.wall*10, initial.wall*10};
 
 	int number_of_atom[3] = {10,10,10};
 	Sys_atom *argon = new Sys_atom(wall, number_of_atom, sigma_Ar, epsilon_Ar, mass_Ar, initial.dtime, initial.r_kr*sigma_Ar);
+	
 	
 	int step = 0;
 	int percentage = 0;
@@ -246,6 +253,7 @@ int main(int argc, char* argv[]){
 	fprintf(energy_fluct, "%d %lf\n", dtime, E_max-E_min);
 	fclose(energy_fluct);
 */
+
 	fclose(foutput);
 	fclose(fenergy);
 	delete argon;
